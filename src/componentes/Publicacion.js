2c241528b0481d87d74ec1c73f2684e56f0eddd0
Sergio Fierro <55807248+SergioF21@users.noreply.github.com>
2023-06-12 23:52:04 -0500

    archivos añadidos de react para el frontend
    no hay garantías de que arranque como react al ser añadidos a la fuerza, no enlazado con las rutas.

---
+++ src/componentes/Publicacion.js
@@ -0,0 +1,32 @@
+import React from "react";
+import '../stylesheets/Publicacion.css'
+function Publicacion (props,{setNumLike,numlike}) {
+    return(
+    <div className="contenedor-publicacion">
+        <img className="imagen-planta" 
+    src={require(`../imagenes/${props.imagen}.jpg`)}
+    alt="planta"></img>
+    <div className="contenedor-texto-planta">
+        <p className="nombre-planta">{props.nombre}</p>
+        <p className="texto-planta">{props.texto}</p>
+    </div>
+    <div className="contenedor-boton">
+        <button
+            className="Like"
+            onClick={setNumLike}>
+                Like
+        </button>
+        <div className='contador'>
+            {numlike}
+        </div>
+    </div>
+    <div className="contenedor-comentarios">
+        {props.comentarios}
+    </div>
+    </div>
+    
+    )
+}
+
+
+export default Publicacion;
